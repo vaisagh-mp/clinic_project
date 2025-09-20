ea0709b6eb905c2c391eaedaee4ddd1560cd503a
PontusDigimark <contact@pontusdigi.com>
2025-09-20 12:59:06 +0530

    20/09/25

--- clinic_project/settings.py
+++ clinic_project/settings.py
@@ -118,12 +118,24 @@ SIMPLE_JWT = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'clinic',
+        'USER': 'clinic',
+        'PASSWORD': 'clinic',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
