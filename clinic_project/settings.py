c7fc0a3f8153da8b09a96caf61a86e5466bbafee
vaisagh-mp <vivekmpcat@gmail.com>
2025-09-28 22:46:05 +0530

    appoinment fix

--- clinic_project/settings.py
+++ clinic_project/settings.py
@@ -118,23 +118,23 @@ SIMPLE_JWT = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'clinic',
-#         'USER': 'clinic',
-#         'PASSWORD': 'clinic',
-#         'HOST': 'localhost',
-#         'PORT': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'clinic',
+        'USER': 'clinic',
+        'PASSWORD': 'clinic',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 
